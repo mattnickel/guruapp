94d371bf6026a7dd91e86f0a259c5d7994e30357
Matt Nickel <mattnickel05@gmail.com>
2020-06-16 14:09:12 -0700

    edit function

--- app/controllers/training_modules_controller.rb
+++ app/controllers/training_modules_controller.rb
@@ -1,6 +1,7 @@
 class TrainingModulesController < ApplicationController
 
   def index
+  	@training_module = TrainingModule.all
   end
 
   def show
@@ -8,14 +9,40 @@ class TrainingModulesController < ApplicationController
   end
 
   def new
+  	@training_module = TrainingModule.new
+  end
+
+  def edit
+  	@training_module = TrainingModule.find(params[:id])
+  end
+
+  def update
+  	@training_module = TrainingModule.find(params[:id])
+    if @training_module.update(training_module_params)
+      redirect_to @training_module
+    else
+      render 'edit'
+    end
   end
 
   def create
 	  @training_module = TrainingModule.new(training_module_params)
+
+	  if @training_module.save
+    	redirect_to @training_module
+	  else
+	    render 'new'
+	  end
 	 
-	  @training_module.save
-	  redirect_to @training_module
-	end
+  end
+  
+  def destroy
+  @training_module = TrainingModule.find(params[:id])
+  @article.destroy
+ 
+  redirect_to articles_path
+end
+
  
 private
   def training_module_params
