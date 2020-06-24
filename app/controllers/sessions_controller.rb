d1650476be3530efec2035c6e8598c0777eebdb3
Matt Nickel <mattnickel05@gmail.com>
2020-06-24 16:58:03 -0700

    login and routing working

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -10,7 +10,7 @@ class SessionsController < ApplicationController
 
 	  if user && user.authenticate(params[:password])
 	    session[:user_id] = user.id
-	    redirect_to root_url, notice: "Logged in!"
+	    redirect_to training_modules_path, notice: "Logged in!"
 	  else
 	    flash.now.alert = "Email or password is invalid"
 	    render "new"
--- config/routes.rb
+++ config/routes.rb
@@ -2,11 +2,14 @@ Rails.application.routes.draw do
   resources :emails
   resources :users
   resources :sessions
+  resources :welcome, path: "home"
 
   get 'sessions/new'
   get 'sessions/index'
 
-  get 'welcome/index', to: 'welcome#index', as: 'home'
+
+  get 'welcome/index'
+  get 'welcome/download', to: 'welcome#download', as: 'download'
   get 'signup', to: 'users#new', as: 'signup'
   get 'login', to: 'sessions#new', as: 'login'
   get 'logout', to: 'sessions#destroy', as: 'logout'
@@ -18,6 +21,6 @@ Rails.application.routes.draw do
     resources :comments
   end
 
-  root 'welcome#index'
+  root 'welcome#index', as: 'home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
