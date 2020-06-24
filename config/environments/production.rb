527809ecd223f8aedf432539d72a94802042b159
Matt Nickel <mattnickel05@gmail.com>
2020-06-24 15:46:06 -0700

    config for prod images

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -28,7 +28,7 @@ Rails.application.configure do
   config.serve_static_assets = false
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
-  config.assets.compile = false
+  config.assets.compile = true
 
   # Generate digests for assets URLs
   config.assets.digest = true
