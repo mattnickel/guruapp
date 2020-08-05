562527ffeb011bc8b4e15d02bfbc83099e7ce74c
Matt Nickel <mattnickel05@gmail.com>
2020-08-05 12:06:54 -0700

    add url to video api

--- app/serializers/video_serializer.rb
+++ app/serializers/video_serializer.rb
@@ -1,3 +1,3 @@
 class VideoSerializer < ActiveModel::Serializer
-   attributes :id, :title, :author, :description, :vimeo_id, :seconds
+   attributes :id, :title, :author, :description, :vimeo_id, :seconds, :url
 end
