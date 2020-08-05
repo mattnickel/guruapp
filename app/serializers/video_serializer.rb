87e656bd49ab536094b133813f1c2cd0288e27dd
Matt Nickel <mattnickel05@gmail.com>
2020-08-05 08:52:29 -0700

    fixing db error

--- app/serializers/video_serializer.rb
+++ app/serializers/video_serializer.rb
@@ -1,3 +1,3 @@
 class VideoSerializer < ActiveModel::Serializer
-   attributes :id, :title, :author, :description, :vimeo_id, :duration
+   attributes :id, :title, :author, :description, :vimeo_id, :seconds
 end
