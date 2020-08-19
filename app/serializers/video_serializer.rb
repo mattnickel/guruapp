dda16d87bc560b25e9f904b7881a0121b60e015b
Matt Nickel <mattnickel05@gmail.com>
2020-08-19 13:42:42 -0700

    added image to api

--- app/serializers/video_serializer.rb
+++ app/serializers/video_serializer.rb
@@ -1,3 +1,10 @@
 class VideoSerializer < ActiveModel::Serializer
-   attributes :id, :title, :author, :description, :vimeo_id, :seconds, :url
+	 
+	 include Rails.application.routes.url_helpers 
+
+   attributes :id, :title, :author, :description, :vimeo_id, :seconds, :url, :image
+
+   def image
+   		object.image.service_url if object.image.attached?
+   end
 end
