65a10f5c2b53c81f34699745a4347fcd0b8bf9da
Matt Nickel <mattnickel05@gmail.com>
2020-09-02 16:34:18 -0700

    changed Application to Active Record for a hail mary fix

--- app/models/api_key.rb
+++ app/models/api_key.rb
@@ -1,4 +1,4 @@
-class APIKey < ApplicationRecord::Base
+class APIKey < ActiveRecord::Base
 	attr_accessible :access_token, :expires_at, :user_id, :active, :application
   before_create :generate_access_token
   before_create :set_expiration
