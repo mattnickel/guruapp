248e00711170e9081affa9991dc0bb8bde034cb6
Matt Nickel <mattnickel05@gmail.com>
2020-09-02 16:38:54 -0700

    removed Base in ApplicationRecord

--- app/models/api_key.rb
+++ app/models/api_key.rb
@@ -1,4 +1,4 @@
-class APIKey < ActiveRecord::Base
+class APIKey < ApplicationRecord
 	attr_accessible :access_token, :expires_at, :user_id, :active, :application
   before_create :generate_access_token
   before_create :set_expiration
