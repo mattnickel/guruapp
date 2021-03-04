17826ed6b798b18f702c430bea2b69247adc838b
mattnickel <mattnickel05@gmail.com>
2021-03-04 15:23:31 -0800

    photos working

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -43,10 +43,10 @@ Rails.application.configure do
   # Compress CSS using a preprocessor.
   # config.assets.css_compressor = :sass
   # Disable Rails's static asset server (Apache or nginx will already do this)
-  config.serve_static_assets = false
+  config.serve_static_assets = true
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
-  config.assets.compile = false
+  config.assets.compile = true
 
   # Generate digests for assets URLs
   config.assets.digest = true
