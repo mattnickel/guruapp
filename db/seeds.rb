b9653d964fe0ec2d5465932c615261d804ada814
mattnickel <mattnickel05@gmail.com>
2021-04-12 16:01:03 -0700

    add user seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+user = User.new
+user.email = 'test@test.com'
+user.username = 'Tester'
+user.password ='123456'
+user.role = 'admin'
+user.save!
