364480f8b18f3a368880f0c4db2ce8309677261c
Naomi Cortez <naozette@gmail.com>
2021-06-23 18:46:01 +0800

    Updated code to specify day

--- app/jobs/active_users_job.rb
+++ app/jobs/active_users_job.rb
@@ -4,13 +4,12 @@ class ActiveUsersJob < ApplicationJob
   def perform(*args)
     # Do something later
     active_users = UserActivity.where('active_count > 0').count
-	stats = Stat.new
-	stats.event_stat = active_users
-	stats.description = 'Number of active users'
-	d = Date.today
-	weekday = Date.parse( '#{d.day} #{d.month} #{d.year}' ).strftime('%A') 
-	stats.day = weekday
-	stats.save
+		stats = Stat.new
+		stats.event_stat = active_users
+		stats.description = 'Number of active users'
+		weekday = Date.today.strftime('%A') 
+		stats.day = weekday
+		stats.save
 	#AtiveUsersJob.set(wait: 1.day).perform_later()
   end
 end
--- app/jobs/inactive_users_job.rb
+++ app/jobs/inactive_users_job.rb
@@ -4,14 +4,13 @@ class InactiveUsersJob < ApplicationJob
   def perform(*args)
     # Do something later
     inactive_users = UserActivity.where('active_count = 0').count
-	stats = Stat.new
-	stats.event_stat = inactive_users
-	stats.description = 'Number of inactive users'
-	stats.created_at = Date.today
-	d = Date.today
-	weekday = Date.parse( '#{d.day} #{d.month} #{d.year}' ).strftime('%A') 
-	stats.day = weekday
-	stats.save
+	  stats = Stat.new
+	  stats.event_stat = inactive_users
+	  stats.description = 'Number of inactive users'
+   	stats.created_at = Date.today
+		weekday = Date.today.strftime('%A') 
+		stats.day = weekday
+	  stats.save
 	#InactiveUsersJob.set(wait: 1.day).perform_later()
   end
 end
