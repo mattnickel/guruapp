5821b3fddba7ac4a3051578bedbd1fd2c680d876
Naomi Cortez <naomi@topoflineinc.com>
2021-06-23 19:38:52 +0800

    Update to to track daily stat

--- app/jobs/number_of_attempted_shares_to_social_media_job.rb
+++ app/jobs/number_of_attempted_shares_to_social_media_job.rb
@@ -3,12 +3,20 @@ class NumberOfAttemptedSharesToSocialMediaJob < ApplicationJob
 
   def perform(*args)
     # Do something later
-    social_attempt = SocialAttempt.where('created_at > ?', Date.today-1.week).count
-	  weekly_stats = WeeklyStat.new
-	  weekly_stats.event_stat = social_attempt
-	  weekly_stats.description = 'Number of attempted shares to social media this week'
-	  weekly_stats.created_at = Date.today
+    
+    social_attempt = SocialAttempt.where('created_at >= ?', 1.days.ago).count
+	  stats = Stat.new
+	  stats.event_stat = social_attempt
+	  stats.description = 'Number of attempted shares'
+	  stats.created_at = Date.today
+	  stats.save
 
-	  weekly_stats.save
+		# social_attempt = SocialAttempt.where('created_at > ?', Date.today-1.week).count
+	  # weekly_stats = WeeklyStat.new
+	  # weekly_stats.event_stat = social_attempt
+	  # weekly_stats.description = 'Number of attempted shares to social media this week'
+	  # weekly_stats.created_at = Date.today
+	  # weekly_stats.save
+
   end
 end
