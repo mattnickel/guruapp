5c3705fdd7ed62416a9b52acf85ef89238c98330
Naomi Cortez <naomi@topoflineinc.com>
2021-07-07 17:37:26 +0800

    Updated create logic for game scores

--- app/controllers/api/v1/game_scores.rb
+++ app/controllers/api/v1/game_scores.rb
@@ -22,22 +22,23 @@ module API
         
         desc "Create"
         params do
-          requires :game_type, score: Integer          
+          requires :game_type          
         end
 
         post do
           current_user = User.find_by(authentication_token: headers['Token'])   
+          game_score = GameScore.new
           game_score.user_id = current_user.id
           game_score.game_type = params[:game_type]       
           game_score.score = 0;
           game_score.created_at = Date.today
           game_score.save
 
-          game_score = GameScore.where(["game_type = :game_type AND user_id = :user_id AND score = :score ", {game_type: params[:game_type], user_id:current_user.id, score: 0}])
-          
-          if GameScore.create!(user_id: current_user.id, score: 0, game_type:game_type, created_at: Date.today)
+          high = GameScore.where(["game_type = :game_type AND updated_at < :today", {game_type: params[:game_type], today: Date.today}]);
+          if GameScore.create!(user_id: current_user.id, score: 0, game_type: params[:game_type], created_at: Date.today)
             render json: {
                 game_id: game_score.id,
+                high:high,
                 is_success: true,
                 status: :ok
             }
