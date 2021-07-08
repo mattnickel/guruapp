8788db5f10326d4c67627d0afa75f83db051a294
Rey <reybatacjr@gmail.com>
2021-07-08 19:12:22 +0800

    update get_max_score method

--- app/controllers/api/v1/game_scores.rb
+++ app/controllers/api/v1/game_scores.rb
@@ -20,8 +20,8 @@ module API
           game_score.created_at = DateTime.current
           game_score.save
 
-          high = GameScores.get_max_score(params[:game_type], "no");
-          today = GameScores.get_max_score(params[:game_type], "yes");
+          high = GameScores.get_max_score(params[:game_type]);
+          today = GameScores.get_max_score(params[:game_type], true);
           render json: {
             game_id: game_score.id,
             high: high,
@@ -48,8 +48,8 @@ module API
           requires :game_type
         end
         get "", :GameScoreSerializer do
-          high = GameScores.get_max_score(params[:game_type], "no");
-          today = GameScores.get_max_score(params[:game_type], "yes");
+          high = GameScores.get_max_score(params[:game_type]);
+          today = GameScores.get_max_score(params[:game_type], true);
 
           render json: {
             high: high,
@@ -68,8 +68,8 @@ module API
           game_score = GameScore.where(["game_type = :game_type AND user_id = :user_id", {game_type: params[:game_type], user_id:current_user.id}])
           new_score = game_score[0].score + params[:score].to_i
           if game_score.update({score: new_score, updated_at: Date.today})
-            high = GameScores.get_max_score(params[:game_type], "no");
-            today = GameScores.get_max_score(params[:game_type], "yes");
+            high = GameScores.get_max_score(params[:game_type]);
+            today = GameScores.get_max_score(params[:game_type], true);
 
             render json: {
               high: high,
@@ -82,8 +82,14 @@ module API
           end
         end
 
-        def get_max_score(game_type, today)
-          sql = "game_type = :game_type AND CAST(game_scores.updated_at as date) #{(today == "yes" ? "=" : "!=")} :today";
+        def get_max_score(game_type, today = false)
+          if(today)
+            sql = "game_type = :game_type AND CAST(game_scores.updated_at as date) = :today";
+          else
+            sql = "game_type = :game_type AND CAST(game_scores.updated_at as date) != :today";
+            # sql = "game_type = :game_type";
+          end
+
           GameScore.joins(:user)
             .where([sql, {game_type: game_type, today: Date.today}])
             .select("users.username", "score")
