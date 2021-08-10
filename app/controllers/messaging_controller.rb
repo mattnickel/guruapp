aa3d7fdc701b5c762981a34a78dcedc7b935b0c0
Naomi Cortez <naomi@topoflineinc.com>
2021-08-10 17:00:36 +0800

    added authentication

--- app/controllers/messaging_controller.rb
+++ app/controllers/messaging_controller.rb
@@ -1,5 +1,5 @@
 class MessagingController < ApplicationController
-  
+  before_action :authenticate_user!
   def index
    @users = User.all
   end
--- app/controllers/my_channel_view_controller.rb
+++ app/controllers/my_channel_view_controller.rb
@@ -1,5 +1,5 @@
 class MyChannelViewController < ApplicationController
-
+  before_action :authenticate_user!
   def index
   	@username = current_user.username
   end
