e6be0899d75969ec90df572a716c078ef47d0508
naomicz <naomi@topoflineinc.com>
2021-10-04 13:28:26 +0800

    mount assessment

--- app/controllers/api/v1/base.rb
+++ app/controllers/api/v1/base.rb
@@ -19,7 +19,7 @@ module API
       mount API::V1::GameScores
       mount API::V1::Dashboard
       #mount API::V1::MyChannelView
-
+      mount API::V1::Assessments
       helpers do
         def authenticate!
           error!('Unauthorized. Invalid or expired token.', 401) unless current_user
