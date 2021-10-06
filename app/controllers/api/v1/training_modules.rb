dc2e8e9d8a29b1c618e75a122009e66279280f6c
naomicz <naomi@topoflineinc.com>
2021-10-06 22:07:21 +0800

    uncomment training modules

--- app/controllers/api/v1/training_modules.rb
+++ app/controllers/api/v1/training_modules.rb
@@ -4,26 +4,26 @@ module API
       include API::V1::Defaults
       include API::V1::Authenticate
       
-      # resource :training_modules do
-      #   desc "Return all training module"
-      #   get "", :TrainingModulesSerializer do
-      #     TrainingModule.all
-      #   end
+      resource :training_modules do
+        desc "Return all training module"
+        get "", :TrainingModulesSerializer do
+          TrainingModule.all
+        end
 
-      #   desc "Return a training module"
-      #   params do
-      #     requires :id, type: String, desc: "id of training module"
-      #   end
-      #   get ":id" do
-      #     TrainingModule.where(id: permitted_params[:id]).first!
-      #   end
-      #   get "random" do
-      #    TrainingModule.order('RANDOM()').first
-      #   end
-      # end
-      # resource :random do 
-      #   TrainingModule.order('RANDOM()').first
-      # end
+        desc "Return a training module"
+        params do
+          requires :id, type: String, desc: "id of training module"
+        end
+        get ":id" do
+          TrainingModule.where(id: permitted_params[:id]).first!
+        end
+        get "random" do
+         TrainingModule.order('RANDOM()').first
+        end
+      end
+      resource :random do 
+        TrainingModule.order('RANDOM()').first
+      end
     end
   end
 end
