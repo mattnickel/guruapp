5f43f62bf3dbf98370c9500b9e8fc1bbd814e71f
naomicz <naomi@topoflineinc.com>
2021-10-06 22:04:41 +0800

    video upload fix

--- app/controllers/videos_controller.rb
+++ app/controllers/videos_controller.rb
@@ -1,43 +1,29 @@
 class VideosController < ApplicationController
-  # before_action :authenticate_user!
-  # protect_from_forgery 
-  #  with: :null_session, only: [:create]
-  # # before_filter :authenticate_user!
-  # before_action :verify_authenticity_token
-  # skip_before_action :verify_authenticity_token
-  # skip_before_action :authenticate_user, only: [:create], raise: false
-  #before_action :authenticate_user!, except: :create
-
+  before_action :set_video, only: %i[ show edit update destroy ]
+  before_action :authenticate_user!, except: [:index, :show]
+  before_action :correct_user, only:[:edit, :update, :destroy]
+  
   def index
   	@videos = Video.all
   end
 
   def show
-    @videos = Video.find(params[:id])
+    # @videos = Video.find(params[:id])
   end
 
   def new
-  	@videos = Video.new
+  	# @videos = Video.new
+    #@video = Video.new
+    @video = current_user.videos.build
   end
 
   def edit
-  	@videos = Video.find(params[:id])
+  	# @videos = Video.find(params[:id])
   end
 
-  def update
-  	@videos = Video.find(params[:id])
-    render 'edit'
-  end
 
   def create
-    
-   
-    # @current_user ||= User.find_by(authentication_token: headers['Token'])
-    # @authentication_token = current_user.authentication_token
-    #getting actiondispatch issue with below code
-	  #  video = Video.new(video_params)
-    #hard-code params
-    @video = Video.new(video_params)
+	  @video = current_user.videos.build(video_params)
 
     respond_to do |format|
       if @video.save
@@ -48,34 +34,48 @@ class VideosController < ApplicationController
         format.json { render json: @video.errors, status: :unprocessable_entity }
       end
     end
-  
+  end
+
 
     # @training_module.user_id = current_user.id
 	  # if @training_module.save
     # 	redirect_to @training_module
 	  # else
 	  #   render 'new'
-  	# end
+	  # end
+
+  def update
+    respond_to do |format|
+      if @video.update(video_params)
+        format.html { redirect_to @video, notice: "Video was successfully updated." }
+        format.json { render :show, status: :ok, location: @video }
+      else
+        format.html { render :edit, status: :unprocessable_entity }
+        format.json { render json: @video.errors, status: :unprocessable_entity }
+      end
+    end
   end
 
+
   def destroy
-    @video = Video.find(params[:id])
     @video.destroy
-   
-    redirect_to video_path
+    respond_to do |format|
+      format.html { redirect_to video_url, notice: "video was successfully destroyed." }
+      format.json { head :no_content }
+    end
+  end
+
+  def correct_user
+    @video = current_user.videps.find_by(id: params[:id])
+    redirect_to videos_path, notice: "Not authorized" if @video.nil?
   end
  
   private
-   
+    def set_video
+      @video = Video.find(params[:id])
+    end
 
-      def video_params
-        params.require(:video).permit(:description, :title, :video)
-        # params.require(:video).permit(:title, :description, :author, :seconds, :image, :video)
-        
-      
-
-      end
-
-  end
-
- 
+    def video_params
+      params.require(:video).permit(:title, :description, :author, :seconds, :image, :video)
+    end
+end
