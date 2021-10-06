7958e635476ddcd0dd5d935e7daf950cb9adaebb
naomicz <naomi@topoflineinc.com>
2021-10-06 16:54:48 +0800

    change and update video controllers

--- app/controllers/api/v1/videos.rb
+++ app/controllers/api/v1/videos.rb
@@ -28,8 +28,80 @@ module API
           Video.where(id: permitted_params[:id]).first!
         end
         
+        desc "Post a new Video"
+        params do
+          requires :title, type: String
+          requires :description, type: String
+          requires :video
+          optional :image
+          optional :seconds, type: Integer
+          optional :social_image
+        end
+        post do
+          # current_user = User.find_by(authentication_token: headers['Token'])
+          # author = current_user.username
+          @current_user ||= User.find_by(authentication_token: headers['Token'])
+          author = @current_user.username
+          save_activity(@current_user)
+          video = ActionDispatch::Http::UploadedFile.new(params[:video])
+          image = ActionDispatch::Http::UploadedFile.new(params[:image])
+
+          # if video = Video.create!(user_id: current_user.id, video: video, image: image,  title: params[:title], description: params[:description], author: author, created_at: DateTime.current)
+          if video = Video.create!(user: current_user, video: video, image: image,  title: params[:title], description: params[:description], author: author, created_at: DateTime.current)
+          render json: {
+              is_success: true,
+              user: current_user.username,
+              status: :ok,
+              video: video
+            }
+          end
+        end
+
+
+        desc "Edit Video Details"
+        params do
+          
+          optional :image
+          optional :social_image
+          optional :title
+          optional :description
+        end
         
+        put do 
+          current_user = User.find_by(authentication_token: headers['Token'])
+          video = Video.where(id: params[:id], user_id: current_user.id)
+
+          if video.update(title: params[:title], description: params[:description], updated_at: DateTime.current)
+            save_activity(current_user)
+            render json: {
+                is_success: true,
+                status: :ok,
+                message: "Video Details Updated",
+                video: video
+              }
+          end
+        end
+
+        desc 'Delete Video'
+        params do
+          requires :id
+        end
+        delete do
+          current_user = User.find_by(authentication_token: headers['Token'])
+           video = Video.where(id: params[:id], user_id: current_user.id)
+
+           if video.find(params[:id]).destroy
+            save_activity(current_user)
+            render json: {
+              is_success: true,
+               status: :ok,
+               message: "Video Succefully Deleted"
+             }
+          end
+        end
+
       end
+
     end
   end
 end
--- app/controllers/videos_controller.rb
+++ app/controllers/videos_controller.rb
@@ -1,49 +1,81 @@
 class VideosController < ApplicationController
-
- 
+  before_action :set_video, only: %i[ show edit update destroy ]
+  before_action :authenticate_user!, except: [:index, :show]
+  before_action :correct_user, only:[:edit, :update, :destroy]
   
   def index
   	@videos = Video.all
   end
 
   def show
-    @videos = Video.find(params[:id])
+    # @videos = Video.find(params[:id])
   end
 
   def new
-  	@videos = Video.new
+  	# @videos = Video.new
+    #@video = Video.new
+    @video = current_user.videos.build
   end
 
   def edit
-  	@videos = Video.find(params[:id])
+  	# @videos = Video.find(params[:id])
   end
 
-  def update
-  	@videos = Video.find(params[:id])
-      render 'edit'
+
+  def create
+	  @video = current_user.videos.build(video_params)
+
+    respond_to do |format|
+      if @video.save
+        format.html { redirect_to @video, notice: "Video was successfully created." }
+        format.json { render :show, status: :created, location: @video }
+      else
+        format.html { render :new, status: :unprocessable_entity }
+        format.json { render json: @video.errors, status: :unprocessable_entity }
+      end
     end
   end
 
-  def create
-	  @videos = Video.new(video_params)
-    @training_module.user_id = current_user.id
-	  if @training_module.save
-    	redirect_to @training_module
-	  else
-	    render 'new'
-	end
+
+    # @training_module.user_id = current_user.id
+	  # if @training_module.save
+    # 	redirect_to @training_module
+	  # else
+	  #   render 'new'
+	  # end
+
+  def update
+    respond_to do |format|
+      if @video.update(video_params)
+        format.html { redirect_to @video, notice: "Video was successfully updated." }
+        format.json { render :show, status: :ok, location: @video }
+      else
+        format.html { render :edit, status: :unprocessable_entity }
+        format.json { render json: @video.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
 
   def destroy
-    @video = Video.find(params[:id])
     @video.destroy
-   
-    redirect_to video_path
+    respond_to do |format|
+      format.html { redirect_to video_url, notice: "video was successfully destroyed." }
+      format.json { head :no_content }
+    end
   end
 
-
+  def correct_user
+    @video = current_user.videps.find_by(id: params[:id])
+    redirect_to videos_path, notice: "Not authorized" if @video.nil?
+  end
  
-private
-  def video_params
-    params.require(:video).permit(:title, :description, :author, :seconds, :image, :video)
-  end
+  private
+    def set_video
+      @video = Video.find(params[:id])
+    end
+
+    def video_params
+      params.require(:video).permit(:title, :description, :author, :seconds, :image, :video)
+    end
 end
