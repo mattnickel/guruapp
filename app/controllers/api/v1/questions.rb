4394b567004002b3c019490b0e0060ba6030089d
naomicz <naomi@topoflineinc.com>
2021-10-12 15:54:17 +0800

    update  questions endpoint

--- app/controllers/api/v1/questions.rb
+++ app/controllers/api/v1/questions.rb
@@ -10,63 +10,66 @@ module API
                   Question.all
                 end
 
-                # desc "Create Assessment"
-                #     params do
-                #         requires :assessment_type
-                #         requires :name
-                #         #requires :assessment_question
-                #     end
+                desc "Create Question"
+                    params do
+                        requires :text
+                        # requires :offered_response
+                    end
 
-                # post do 
-                #     current_user = User.find_by(authentication_token: headers['Token'])  
-                #     assessment = Assessment.new
-                #     assessment.name = params[:name]
-                #     assessment.assessment_type = params[:assessment_type]
-                #     assessment.created_at = DateTime.current
-                #     assessment.save
-                #     render json: {
-                #         assessment_id: assessment.id,
-                #         assessment_type: assessment.assessment_type,
-                #         is_success: true,
-                #         status: :ok
-                #     }
-                #     current_user = User.find_by(authentication_token: headers['Token'])
-                #     if @assessment = Assessment.create!({name: params[:name], assessment_type: params[:assessment_type], created_at: DateTime.current})
-                #         render json: {
-                #         assessment_id: assessment.id,
-                #         name: assessment.name,
-                #         assessment_type: assessment.assessment_type,
-                #         is_success: true,
-                #         status: :ok
-                #         }
-                #     else
-                #         status 400
-                #     end
-                # end
+                    post do 
+                        current_user = User.find_by(authentication_token: headers['Token'])
+                        if question = Question.create!({text: params[:text], created_at: DateTime.current})
+                            render json: {
+                            question_id: question.id,
+                            text: question.text,
+                            is_success: true,
+                            status: :ok
+                            }
+                        else
+                            status 400
+                        end
+                    end
 
-                # desc "Update Assessment"
-                # if assessment.update({name: params[:name], assessment_type: params[:assessment_type], updated_at: DateTime.current})
-                #     render json: {
-                #         assessment_id: assessment.id,
-                #         name: assessment.name,
-                #         assessment_type: assessment.assessment_type,
-                #         is_success: true,
-                #         status: :ok
-                #     }
-                # else
-                #     status 400
-                # end
+                desc "Update Question"
+                params do
+                    requires :text
+                    requires :id
+                    # requires :offered_response
+                end
+                put do
+
+                    current_user = User.find_by(authentication_token: headers['Token'])
+                    question = Question.find_by(id: params[:id])
+                    if question.update({text: params[:text], updated_at: DateTime.current})
+                    render json: {
+                        question_id: question.id,
+                        text: question.text,
+                        is_success: true,
+                        status: :ok
+                    }
+                    else
+                        status 400
+                    end
+                end
+                
 
 
-                # desc "Delete Assessment"
-                # params do
-                #     requires :id, desc: 'Assessment ID.'
-                # end
+                desc "Delete Question"
+                params do
+                    requires :id, desc: 'Question ID.'
+                end
 
-                # delete ':id' do
-                #     authenticate!
-                #     assessment.find(params[:id]).destroy
-                # end
+                delete do
+
+                   if Question.find(params[:id]).destroy
+                    render json: {
+                        is_success: true,
+                        status: :ok
+                }
+                   else
+                      status 400
+                   end
+                end
             end
 
         end
