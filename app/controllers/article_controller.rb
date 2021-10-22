d522c9862168dff1153778faa8f7850af96da1b4
naomicz <naomi@topoflineinc.com>
2021-10-22 22:02:46 +0800

    rewrite article edit function

--- app/controllers/article_controller.rb
+++ app/controllers/article_controller.rb
@@ -19,12 +19,19 @@ class ArticleController < ApplicationController
       end
   
       def edit
-          @article = Article.find(params[:id])
+
       end
     
       def update
-          @article = Article.find(params[:id])
-          render 'edit'
+        respond_to do |format|
+          if @article.update(article_params)
+            format.html { redirect_to @article, notice: "article was successfully updated." }
+            format.json { render :show, status: :ok, location: @article }
+          else
+            format.html { render :edit, status: :unprocessable_entity }
+            format.json { render json: @article.errors, status: :unprocessable_entity }
+          end
+        end
       end
   
       def create
